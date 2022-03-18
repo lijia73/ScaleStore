62f6ff21c51bf5e9086ce3502767a3947dbd297b
lijia <gaillee666@outlook.com>
2022-03-18 17:44:03 +0800

    fix error

--- src/server.h
+++ src/server.h
@@ -5,24 +5,26 @@
 #include "server_mm.h"
 #include "kv_utils.h"
 
-class Server {
+class Server
+{
     uint32_t server_id_;
     volatile uint8_t need_stop_;
-    UDPNetworkManager * nm_;
-    ServerMM          * mm_;
-    
+    UDPNetworkManager *nm_;
+    ServerMM *mm_;
+
 public:
-    Server(const struct GlobalConfig * conf);
+    Server(const struct GlobalConfig *conf);
     ~Server();
 
-    int server_on_connect(const struct KVMsg * request, 
-        struct sockaddr_in * src_addr, socklen_t src_addr_len);
-    int server_on_alloc(const struct KVMsg * request, 
-        struct sockaddr_in * src_addr, socklen_t src_addr_len);
-    int server_on_alloc_subtable(const struct KVMsg * request,
-        struct sockaddr_in * src_addr, socklen_t src_addr_len);
-
-    void * thread_main();
+    int server_on_connect(const struct KVMsg *request,
+                          struct sockaddr_in *src_addr, socklen_t src_addr_len);
+    int server_on_alloc(const struct KVMsg *request,
+                        struct sockaddr_in *src_addr, socklen_t src_addr_len);
+    int server_on_alloc_subtable(const struct KVMsg *request,
+                                 struct sockaddr_in *src_addr, socklen_t src_addr_len);
+    int server_on_free(const struct KVMsg *request, struct sockaddr_in *src_addr,
+                       socklen_t src_addr_len);
+    void *thread_main();
 
     void stop();
 
@@ -31,11 +33,12 @@ public:
     uint64_t get_subtable_st_addr();
 };
 
-typedef struct TagServerMainArgs {
-    Server * server;
-    int      core_id;
+typedef struct TagServerMainArgs
+{
+    Server *server;
+    int core_id;
 } ServerMainArgs;
 
-void * server_main(void * server_main_args);
+void *server_main(void *server_main_args);
 
 #endif
